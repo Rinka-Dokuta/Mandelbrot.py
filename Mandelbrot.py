e97f4e7f0a53f74f4f3ee2710083f21df881a84d
Rinka <146360541+Rinka-Dokuta@users.noreply.github.com>
2024-10-02 10:27:40 -0600

    Update Mandelbrot.py

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -22,37 +22,40 @@ def Mandelbrot(c):
 
     return counter
 
-#The Render Algorithm
+The Render Algorithm
 i = 0
 while i < WIDTH:
     i += 1
     j = 0
     while j < HEIGHT:
-        j += 1
-        screen.set_at((i, j), (255, 0, 0)) #Set pixel at (i, j) to red
-        pygame.display.flip()
+        #j += 1
+        #screen.set_at((i, j), (255, 0, 0)) #Set pixel at (i, j) to red
+        
     i += 1
     
 #Updated Drawing Algorithm
     c = complex(i, j)#create a complex number from iterators
     num = Mandelbrot(c)#call the function
+    #these if statements are just to differentiate the colors more, not needed if you want black & white image
     if num < 20:
         screen.set_at((int(i*200+400), int(j*200+400)), (num*8,num*6,num*12))
     elif num < 40:
         screen.set_at((int(i*200+400), int(j*200+400)), (num*2,num/2,num*6))
     elif num < 80:
-        screen.set_at((int(i*200+400), int(j*200+400)), (255, 255, 255))
+        screen.set_at((int(i*200+400), int(j*200+400)), (255, 255, 255)) #white
     else:
         screen.set_at((int(i*200+400), int(j*200+400)), (num*3,num/2,num*2))   
     #print("num is ", num, "at", i*100+400, j*100+400)
     
-i = -2
-while i<2:
-    i+=1
+    pygame.display.flip() #puts pixel on screen
     
-    j = -2
-    while j<2:
-        j+=1
+i = -2 #lower bound for real axis
+while i<2: #upper bound for real (horizontal) axis
+    i+=1 #make this number smaller to increase picture resolution
+    
+    j = -2 #lower bound for imaginary axis
+    while j<2: #upper bound for imaginary (vertical) axis
+        j+=1 #make this number smaller to increase picture resolution
                       
     
 
@@ -70,5 +73,7 @@ while i<2:
 
 pygame.time.wait(10000)#Payse to see the picture
 pygame.quit()#quit pygame
+
+
 
 
