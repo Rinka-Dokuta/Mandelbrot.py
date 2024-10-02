7283e892bd72137604bd8858b07e62d91e6e7a34
Rinka <146360541+Rinka-Dokuta@users.noreply.github.com>
2024-10-02 10:20:20 -0600

    Create Mandelbrot.py

---
+++ Mandelbrot.py
@@ -0,0 +1,74 @@
+import pygame
+import math
+import cmath #This moduel handles complex numbers
+
+pygame.init()
+pygame.display.set_caption("mandelbrot") #window title
+screen = pygame.display.set_mode((800, 800)) #game screen
+screen.fill((0,0,0))
+
+WIDTH = 800
+HEIGHT = 800
+
+
+#Mandelbrot Function
+def Mandelbrot(c):
+    z = 0
+    counter = 0
+
+    while abs(z) < 2 and counter < 80:
+        z = z**2 + c
+        counter += 1
+
+    return counter
+
+#The Render Algorithm
+i = 0
+while i < WIDTH:
+    i += 1
+    j = 0
+    while j < HEIGHT:
+        j += 1
+        screen.set_at((i, j), (255, 0, 0)) #Set pixel at (i, j) to red
+        pygame.display.flip()
+    i += 1
+    
+#Updated Drawing Algorithm
+    c = complex(i, j)#create a complex number from iterators
+    num = Mandelbrot(c)#call the function
+    if num < 20:
+        screen.set_at((int(i*200+400), int(j*200+400)), (num*8,num*6,num*12))
+    elif num < 40:
+        screen.set_at((int(i*200+400), int(j*200+400)), (num*2,num/2,num*6))
+    elif num < 80:
+        screen.set_at((int(i*200+400), int(j*200+400)), (255, 255, 255))
+    else:
+        screen.set_at((int(i*200+400), int(j*200+400)), (num*3,num/2,num*2))   
+    #print("num is ", num, "at", i*100+400, j*100+400)
+    
+i = -2
+while i<2:
+    i+=1
+    
+    j = -2
+    while j<2:
+        j+=1
+                      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+pygame.time.wait(10000)#Payse to see the picture
+pygame.quit()#quit pygame
+
+
